56669dd925ccf74d96a6c12135f2aaeaf4b34825
Jetross <jetrossneri07@gmail.com>
2025-04-11 21:02:21 +0800

    added register form to router

--- agriloop-frontend/src/components/forms/RegisterForm.vue
+++ agriloop-frontend/src/components/forms/RegisterForm.vue
@@ -1,3 +1,51 @@
-<template></template>
+<template>
+  <v-card
+    class="d-flex justify-center align-center"
+    style="height: 100vh; width: 100vw"
+  >
+    <v-card
+      class="mx-auto elevation-5"
+      prepend-icon="$vuetify"
+      subtitle="LoginForm"
+      width="350"
+    >
+      <template v-slot:title>
+        <span class="font-weight-black">Welcome to AgriLoop</span>
+      </template>
+
+      <v-sheet class="mx-auto" width="300">
+        <v-form @submit.prevent="handleLogin">
+          <v-text-field
+            v-model="email"
+            :rules="rules"
+            label="E-mail"
+          ></v-text-field>
+          <v-text-field
+            v-model="password"
+            :rules="rules"
+            :type="showPassword ? 'text' : 'password'"
+            label="Password"
+          >
+            <template v-slot:append-inner>
+              <v-icon
+                @click.stop="togglePasswordVisibility"
+                class="cursor-pointer"
+              >
+                {{ showPassword ? "mdi-eye-off" : "mdi-eye" }}
+              </v-icon>
+            </template>
+          </v-text-field>
+          <v-btn class="mt-2" type="submit" block>Login</v-btn>
+        </v-form>
+        <div class="text-center mt-2 mb-3">
+          <span>
+            Don't have an account?
+            <router-link to="/register">Register</router-link>
+          </span>
+        </div>
+      </v-sheet>
+    </v-card>
+  </v-card>
+</template>
 <script setup></script>
 <style></style>
--- agriloop-frontend/src/router/index.js
+++ agriloop-frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import LoginForm from "@/components/forms/LoginForm.vue"; // adjust path if needed
+import Register from "../components/forms/RegisterForm.vue";
 import Home from "@/components/page/Home.vue"; // your dashboard/homepage
 
 const routes = [
@@ -7,6 +8,19 @@ const routes = [
     path: "/",
     name: "Login",
     component: LoginForm,
+    beforeEnter: (to, from, next) => {
+      const token = localStorage.getItem("auth_token");
+      if (token) {
+        next("/home"); // Redirect to home if the user is already logged in
+      } else {
+        next(); // Allow access to the login page
+      }
+    },
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: Register,
     beforeEnter: (to, from, next) => {
       const token = localStorage.getItem("auth_token");
       if (token) {
