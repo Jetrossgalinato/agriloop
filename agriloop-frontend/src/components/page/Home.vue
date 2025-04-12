a8f0d167c9005b586295f7f576576ef6562a9e02
Jetross <jetrossneri07@gmail.com>
2025-04-12 14:33:13 +0800

    login to home, working

--- agriloop-frontend/src/components/page/Home.vue
+++ agriloop-frontend/src/components/page/Home.vue
@@ -1,5 +1,21 @@
 <template>
-  <h1>Home</h1>
+  <h1>This is the Home page</h1>
+  <v-list-item
+    prepend-icon="mdi-logout"
+    title="Logout"
+    class="sidebar-text"
+    @click="logout"
+  ></v-list-item>
 </template>
-<script setup></script>
+<script setup>
+import { ref, onMounted } from "vue";
+import { useRouter } from "vue-router";
+import axios from "axios";
+const router = useRouter();
+const logout = () => {
+  localStorage.removeItem("auth_token"); // Clear the token
+  localStorage.removeItem("user_id"); // Clear the user ID
+  router.push("/"); // Redirect to login
+};
+</script>
 <style scoped></style>
