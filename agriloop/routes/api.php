2621141f33e1912577d3796189cb5c250b7447bc
Jetross <jetrossneri07@gmail.com>
2025-04-14 00:12:43 +0800

    added UserController to the api route

--- agriloop/routes/api.php
+++ agriloop/routes/api.php
@@ -2,11 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-
+use App\Http\Controllers\UserController;
 
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::get('/test', function () {
-    return response()->json(['message' => 'API is working!']);
-});
+Route::get('/user/{id}', [UserController::class, 'getUserDetails']);
