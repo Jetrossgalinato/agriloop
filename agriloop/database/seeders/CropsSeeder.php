968932083462dddb2f3cfc5c5f0df68d106cb12e
Jetross <jetrossneri07@gmail.com>
2025-04-15 09:19:06 +0800

    images are in public folder and added in the seeders

--- agriloop/database/seeders/CropsSeeder.php
+++ agriloop/database/seeders/CropsSeeder.php
@@ -16,32 +16,32 @@ class CropsSeeder extends Seeder
         Crops::create([
             'name' => 'Wheat',
             'description' => 'Wheat is a cereal grain that is a staple food in many parts of the world.',
-            'image' => 'wheat.jpg',
+            'image' => asset('../agriloop/public/images/wheat.jpg'),
         ]);
         Crops::create([
             'name' => 'Rice',
             'description' => 'Rice is a staple food for more than half of the world\'s population.',
-            'image' => 'rice.jpg',
+            'image' => asset('../agriloop/public/images/rice.jpg'),
         ]);
         Crops::create([
             'name' => 'Corn',
             'description' => 'Corn is a cereal plant that yields large grains, or kernels, set in rows on a cob.',
-            'image' => 'corn.jpg',
+            'image' => asset('../agriloop/public/images/corn.jpg'),
         ]);
         Crops::create([
             'name' => 'Soybean',
             'description' => 'Soybean is a species of legume native to East Asia, widely grown for its edible bean.',
-            'image' => 'soybean.jpg',
+            'image' => asset('../agriloop/public/images/soybean.jpg'),
         ]);
         Crops::create([
             'name' => 'Barley',
             'description' => 'Barley is a cereal grain that is used for food, animal feed, and brewing.',
-            'image' => 'barley.jpg',
+            'image' => asset('../agriloop/public/images/barley.jpg'),
         ]);
         Crops::create([
             'name' => 'Oats',
             'description' => 'Oats are a cereal grain that is commonly used for food and animal feed.',
-            'image' => 'oats.jpg',
+            'image' => asset('../agriloop/public/images/oats.jpg'),
         ]);
     }
 }
