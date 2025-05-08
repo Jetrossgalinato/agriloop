984187797b35ee6f23f2ff63d2423a5417497688
Jetross <jetrossneri07@gmail.com>
2025-05-09 07:47:06 +0800

    removed unimportant files

--- agriloop-frontend/src/plugins/vuetify.js
+++
@@ -1,9 +0,0 @@
-import "vuetify/styles";
-import { createVuetify } from "vuetify";
-import "@mdi/font/css/materialdesignicons.css"; // <- Import MDI
-
-export default createVuetify({
-  icons: {
-    defaultSet: "mdi",
-  },
-});
